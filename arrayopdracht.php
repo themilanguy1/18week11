0b8494a0a55677f4d3f0a5c4136e6a2886c8257a
themilanguy1 <milan.gupta@ymail.com>
2018-03-16 14:34:37 +0100

    w

--- arrayopdracht.php
+++ arrayopdracht.php
@@ -1,25 +1,28 @@
 <?php
 $age = array(
     "Chardonnai"=>array(
-        array("2010","3"),
-        array("2011","5"),
-        array("2011","4")
+        "2010"=>"3",
+        "2010"=>"3",
+        "2011"=>"5",
+        array("2012","4")
         ), 
     "Bordeaux"=>array(
         array("2010","5"),
         array("2011","1"),
-        array("2011","3")
+        array("2012","3")
         ), 
     "Chateau migraine"=>array(
         array("2010","1"),
         array("2011","5"),
-        array("2011","2")
+        array("2012","2")
         )
     );
 
 foreach($age as $x => $x_value) {
     echo $x." ";
-    echo $x_value[1]." ".$x_value[0]."<br>";
+    echo $x_value[0][0]." ".$x_value[0][1]."<br>";
+    echo $x_value[1][0]." ".$x_value[1][1]."<br>";
+    echo $x_value[2][0]." ".$x_value[2][1]."<br>";
 }
 
 ?>
