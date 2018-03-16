ef10d287d572e35c83a11ec33d8329eaa0f5e669
themilanguy1 <milan.gupta@ymail.com>
2018-03-16 14:05:17 +0100

    w

--- arrayopdracht.php
+++ arrayopdracht.php
@@ -1,11 +1,25 @@
 <?php
-$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+$age = array(
+    "Chardonnai"=>array(
+        array("2010","3"),
+        array("2011","5"),
+        array("2011","4")
+        ), 
+    "Bordeaux"=>array(
+        array("2010","5"),
+        array("2011","1"),
+        array("2011","3")
+        ), 
+    "Chateau migraine"=>array(
+        array("2010","1"),
+        array("2011","5"),
+        array("2011","2")
+        )
+    );
 
-foreach($age as $item) {
-    echo $;
-    echo $x_value."<br>";
+foreach($age as $x => $x_value) {
+    echo $x." ";
+    echo $x_value[1]." ".$x_value[0]."<br>";
 }
-https://www.w3schools.com/php/php_arrays.asp
-
 
 ?>
