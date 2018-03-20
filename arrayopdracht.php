c814cc1ae1ba4fae3e5cef96c77294aeaebe1b44
themilanguy1 <milan.gupta@ymail.com>
2018-03-20 09:40:42 +0100

    w

--- arrayopdracht.php
+++ arrayopdracht.php
@@ -45,7 +45,8 @@ $wijn = array(
     echo "<th> </th> <th>2010</th> <th>2011</th> <th>2012</th>";
     foreach ($wijn as $key => $value) {
         echo "<tr>";
-        echo "<td>".$key."</td>";
+        echo "<td>";
+        echo "<a href='?id='$key'>.$key."</a></td>";
         echo "<td>".$wijn[$key]['2010']."</td>";
         echo "<td>".$wijn[$key]['2011']."</td>";
         echo "<td>".$wijn[$key]['2012']."</td>";
@@ -53,6 +54,9 @@ $wijn = array(
     }
 
     echo "</table>";
+
+    function delArray()
+
 ?>
 
 </body>
